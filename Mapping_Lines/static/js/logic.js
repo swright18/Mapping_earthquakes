e76e2ca0c939a9513093ee950055a32ab6acd3df
swright18 <79758494+swright18@users.noreply.github.com>
2021-06-17 22:44:46 -0500

    adding lines

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,8 +1,25 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+// Coordinates for each point to be used in the line.
+// Coordinates for each point to be used in the polyline.
+let line = [
+    [33.9416, -118.4085],
+    [30.1975, -97.6664],
+    [43.6777, -79.6248],
+    [40.6413, -73.7781]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "blue",
+    type: dasharray,
+    weight: 4,
+    opacity: 0.5
+}).addTo(map);
 
 // // Create the map object with a center and zoom level.
 // let map = L.map("mapid", {
@@ -13,26 +30,26 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //   });
 
 // Get data from cities.js
-let cityData = cities;
+// let cityData = cities;
 
-// Loop through the cities array and create one marker for each city.
-// Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000,
-        fillColor: "#deae66",
-        color: '#db8a0f',
-        weight: 3,
-        opacity: 1,
-        fillOpacity: 0.2,
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(map);
-});
+// // Loop through the cities array and create one marker for each city.
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function (city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//         radius: city.population / 100000,
+//         fillColor: "#deae66",
+//         color: '#db8a0f',
+//         weight: 3,
+//         opacity: 1,
+//         fillOpacity: 0.2,
+//     })
+//         .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//         .addTo(map);
+// });
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
